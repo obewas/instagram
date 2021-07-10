fb4f0f029a6729870212a6ce7e9eefd3068f8038
obewas <obewas1202@gmail.com>
2021-07-10 07:52:29 +0300

    added cloudinary

---
+++ clone/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Photo(models.Model):
+    name = models.CharField(max_length=50)
+    image = cloudinary.models.CloudinaryField('image')
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ForeignKey(Photo, on_delete=models.CASCADE)
+    bio = models.TextField()
