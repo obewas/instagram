66572318f097ed6c276ef1487c057616e3eb77b4
obewas <obewas1202@gmail.com>
2021-07-10 19:11:15 +0300

    installed profile card

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -143,5 +143,5 @@ LOGIN_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
-LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'profile'
+LOGOUT_REDIRECT_URL = 'login'
