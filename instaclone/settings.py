4d762b03c0c73d7182717614cc64ab71c04a97c6
obewas <obewas1202@gmail.com>
2021-07-16 04:10:26 +0300

    changed password form

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -151,6 +151,10 @@ EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
 cloudinary.config(
     cloud_name = 'hs8k1napd',
     api_key= '711616426271462',
